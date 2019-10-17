836ebd28f069fcc783c666685215a75c2f5e8bc6
guozhao <davidkoojohn@163.com>
2019-10-17 15:56:58 +0800

    add picker component

--- src/components/vant-component.js
+++ src/components/vant-component.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 
 // 导入组建
-import { Button, Icon } from "vant";
+import { Button, Icon, Picker } from "vant";
 
 // 导入样式
 import "vant/lib/index.css";
@@ -9,3 +9,4 @@ import "vant/lib/index.css";
 // 暴露组建
 Vue.component("van-button", Button);
 Vue.component("van-icon", Icon);
+Vue.component("van-picker", Picker);
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,6 +6,7 @@
     <van-icon name="chat" dot />
     <van-icon name="chat" info="9" />
     <van-icon name="chat" info="99+" />
+    <van-picker :columns="columns" @change="onChange" />
     <h1>{{ title }}</h1>
     <h1>{{ home_title }}</h1>
     <div class="box"></div>
@@ -15,12 +16,23 @@
 
 <script>
 import { mapState } from "vuex";
+import { Toast } from "vant";
 
 export default {
   name: "Home",
   computed: {
     ...mapState(["title"]),
     ...mapState("homeModule", ["home_title"])
+  },
+  data() {
+    return {
+      columns: ["杭州", "宁波", "温州", "嘉兴", "湖州"]
+    };
+  },
+  methods: {
+    onChange(picker, value, index) {
+      Toast(`当前值：${value}, 当前索引：${index}`);
+    }
   }
 };
 </script>
